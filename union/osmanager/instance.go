acb711568c7901a5a1e9550dbe6bb594e24b70ec
Felipe Alafy <69154350+FelipeAlafy@users.noreply.github.com>
2021-11-05 10:21:58 -0300

    BasicStruct now don't crash when union and instances folders already exists.

---
+++ union/osmanager/instance.go
@@ -0,0 +1,61 @@
+package osmanager
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"com.github/FelipeAlafy/union/manager"
+)
+
+func CreateInstanceFolder(c manager.Client) error {
+	if _, err := os.Stat("union/"); os.IsNotExist(err) {
+		osPanic(err)
+	} else if _, err := os.Stat("union/instances"); os.IsNotExist(err) {
+		osPanic(err)
+	}
+	err := os.Mkdir(fmt.Sprint("union/instances/", c.Nome), 0755)
+	osPanic(err)
+	return nil
+}
+
+func NewInstance(c manager.Client) *os.File {
+	path := fmt.Sprint("union/instances/", c.Nome)
+	file, err := os.Create(fmt.Sprint(path, "/", c.Nome, ".json"))
+	osPanic(err)
+	return file
+}
+
+func InstanceData(bytes []byte, file *os.File) {
+	_, err := file.WriteString(string(bytes))
+	osPanic(err)
+}
+
+func GetInstances() []string {
+	var jsons []string
+
+	filepath.Walk("union/instances", 
+		func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+
+			if !info.IsDir() {
+				filename := strings.SplitAfterN(path, "/", 4)
+				if extension := strings.Split(filename[len(filename) - 1], "."); extension[1] == "json" {
+					log.Println("Reading -> ", filename)
+					if err != nil {
+						log.Println(err)
+					}
+					dat, _ := os.ReadFile(path)
+					jsons = append(jsons, string(dat))
+				}
+			}
+			
+			return nil
+		},
+	)
+	return jsons
+}
