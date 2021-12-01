03148a1f6776ee0a5199b500a93c94c2f4a3698d
FelipeAlafy <69154350+FelipeAlafy@users.noreply.github.com>
2021-12-01 12:58:14 -0300

    Photos in windows isn't working. Fixed.

--- union/osmanager/basicStruct.go
+++ union/osmanager/basicStruct.go
@@ -4,9 +4,27 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"runtime"
 )
+var so string
 
 func BasicFolders() (error) {
+	o := runtime.GOOS
+    switch o {
+    case "windows":
+        fmt.Println("Windows")
+		so = "\\"
+    case "darwin":
+        fmt.Println("MAC operating system")
+		so = "/"
+    case "linux":
+        fmt.Println("Linux")
+		so = "/"
+    default:
+        fmt.Printf("%s.\n", o)
+		so = "/"
+    }
+	
 	if _, err := os.Stat("union"); os.IsNotExist(err) {
 		err := os.Mkdir("union", 0755)
 		osPanic(err)
@@ -14,10 +32,10 @@ func BasicFolders() (error) {
 		log.Println("union folder already exist skipping.")
 	}
 	if _, err := os.Stat("union/instances"); os.IsNotExist(err) {
-		err := os.Mkdir("union/instances", 0755)
+		err := os.Mkdir(fmt.Sprint("union", so, "instances"), 0755)
 		osPanic(err)
 	} else {
-		log.Println("union/instances folder already exist skipping.")
+		log.Println("union",so, "instances folder already exist skipping.")
 	}
 	return nil
 }
